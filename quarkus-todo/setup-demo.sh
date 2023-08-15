ccd7cf99879e55016510359dbcf7da7a96a0947d
Eric Deandrea <eric.deandrea@gmail.com>
2023-08-15 13:53:50 -0400

    Removing un-needed stuff

---
+++ quarkus-todo/setup-demo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+# Make all the directories
+echo "Creating all the directories"
+mkdir -p src/main/java/com/acme/todo/client
+mkdir -p src/main/java/com/acme/todo/domain
+mkdir -p src/main/java/com/acme/todo/repository
+mkdir -p src/main/java/com/acme/todo/rest
+mkdir -p src/main/resources/META-INF/resources
+rm -rf src/main/resources/application.properties
+touch src/main/resources/application.yml
+mkdir -p src/test/java/com/acme/todo/client
+mkdir -p src/test/java/com/acme/todo/rest
+
+# Fetch starter files
+echo "Fetching starter files"
+curl -Ls https://raw.githubusercontent.com/edeandrea/quarkus-todo-apps/main/quarkus-todo/src/main/java/com/acme/todo/domain/TodoEntity.java -o src/main/java/com/acme/todo/domain/TodoEntity.java
+curl -Ls https://raw.githubusercontent.com/edeandrea/quarkus-todo-apps/main/quarkus-todo/src/main/java/com/acme/todo/repository/TodoRepository.java -o src/main/java/com/acme/todo/repository/TodoRepository.java
+#curl -Ls https://raw.githubusercontent.com/edeandrea/quarkus-todo-apps/main/quarkus-todo/src/main/resources/application.yml -o src/main/resources/application.yml
+curl -Ls https://raw.githubusercontent.com/edeandrea/quarkus-todo-apps/main/quarkus-todo/src/main/resources/import.sql -o src/main/resources/import.sql
+curl -Ls https://raw.githubusercontent.com/edeandrea/quarkus-todo-apps/main/quarkus-todo/src/main/resources/META-INF/resources/index.html -o src/main/resources/META-INF/resources/index.html
+curl -Ls https://raw.githubusercontent.com/edeandrea/quarkus-todo-apps/main/quarkus-todo/src/main/resources/META-INF/resources/todo-component.html -o src/main/resources/META-INF/resources/todo-component.html
+curl -Ls https://raw.githubusercontent.com/edeandrea/quarkus-todo-apps/main/quarkus-todo/src/main/resources/META-INF/resources/todo-component.js -o src/main/resources/META-INF/resources/todo-component.js
+curl -Ls https://raw.githubusercontent.com/edeandrea/quarkus-todo-apps/main/quarkus-todo/src/main/resources/META-INF/resources/todo.css -o src/main/resources/META-INF/resources/todo.css
+curl -Ls https://raw.githubusercontent.com/edeandrea/quarkus-todo-apps/main/quarkus-todo/src/main/resources/META-INF/resources/todo.js -o src/main/resources/META-INF/resources/todo.js
